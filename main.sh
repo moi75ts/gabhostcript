328e0e4862c701f366f9f50b19dd9448a890ad16
Gabriel <gabrielminecraft51@gmail.com>
2021-02-20 12:18:25 +0100

    Update main.sh
    update

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 yum -y update
-dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
-sudo dnf module reset php
-sudo dnf module enable php:remi-8.0
+dnf install dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+sudo dnf module reset php -y
+sudo dnf module enable php:remi-8.0 -y
 
-sudo dnf install php php-cli php-gd php-mysql php-pdo php-mbstring php-tokenizer php-bcmath php-xml php-fpm php-curl php-zip
-yum install nginx
-yum install git-core
+sudo dnf install php php-cli php-gd php-mysql php-pdo php-mbstring php-tokenizer php-bcmath php-xml php-fpm php-curl php-zip -y
+yum install nginx -y
+yum install git-core -y
 
 firewall-cmd --add-port=80/tcp --permanent
 firewall-cmd --add-port=443/tcp --permanent
