615967abbe28849482dfb15426b35c228f3721f3
Gabriel <gabrielminecraft51@gmail.com>
2021-02-20 11:44:17 +0100

    Update main.sh
    more steps included

--- main.sh
+++ main.sh
@@ -9,5 +9,10 @@ yum install git-core
 firewall-cmd --add-port=80/tcp --permanent
 firewall-cmd --add-port=443/tcp --permanent
 firewall-cmd --reload
-sebool httpd_can_network_connect 1
+setsebool httpd_can_network_connect 1
 setsebool httpd_can_network_connect_db 1
+mkdir /var/www/ -p
+cd /var/www/
+git clone https://github.com/moi75ts/gabhost
+cd gabhost
+mv host.conf /etc/nginx/conf.d/
